1140bc8f143fd6bd35055b71bb13184ef59d1147
ethsgo <code@ethsgo.com>
2021-11-27 19:36:06 +0200

    await deployment

--- scripts/20-01.js
+++ scripts/20-01.js
@@ -3,6 +3,7 @@ const hre = require('hardhat')
 async function main() {
   const Contract = await hre.ethers.getContractFactory('C20_01')
   const contract = await Contract.deploy()
+  await contract.deployed()
 
   console.log(await contract.p1())
 }
--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -3,6 +3,7 @@ const hre = require('hardhat')
 async function main() {
   const Contract = await hre.ethers.getContractFactory('HelloWorld')
   const contract = await Contract.deploy()
+  await contract.deployed()
 
   console.log(await contract.helloWorld())
 }
