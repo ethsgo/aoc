b9e9c46048c2686a26c4c78d97754f9c082d5eba
ethsgo <code@ethsgo.com>
2021-11-30 08:29:58 +0200

    Revert to VSCode defaults

--- contracts/20-01.sol
+++ contracts/20-01.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 import "./Parser.sol";
 
 contract C20_01 is Parser {
-    function main(string memory input) external returns (uint, uint) {
-        uint[] memory xs = parseInts(input);
+    function main(string memory input) external returns (uint256, uint256) {
+        uint256[] memory xs = parseInts(input);
         if (xs.length == 0) {
             xs = parseInts("[1721, 979, 366, 299, 675, 1456]");
         }
@@ -13,9 +13,9 @@ contract C20_01 is Parser {
         return (p1(xs), p2(xs));
     }
 
-    function p1(uint[] memory xs) private pure returns (uint) {
-        for (uint i = 0; i < xs.length; i++) {
-            for (uint j = i + 1; j < xs.length; j++) {
+    function p1(uint256[] memory xs) private pure returns (uint256) {
+        for (uint256 i = 0; i < xs.length; i++) {
+            for (uint256 j = i + 1; j < xs.length; j++) {
                 if (xs[i] + xs[j] == 2020) {
                     return xs[i] * xs[j];
                 }
@@ -26,19 +26,19 @@ contract C20_01 is Parser {
 
     // mappings cannot be created dynamically, you have to assign them
     // from a state variable.
-    mapping(uint => bool) m;
+    mapping(uint256 => bool) m;
 
-    function p2(uint[] memory xs) private returns (uint) {
+    function p2(uint256[] memory xs) private returns (uint256) {
         // mappings cannot be deleted, but it's fine, we only run this part once.
-        for (uint i = 0; i < xs.length; i++) {
+        for (uint256 i = 0; i < xs.length; i++) {
             m[xs[i]] = true;
         }
 
-        for (uint i = 0; i < xs.length; i++) {
-            for (uint j = i + 1; j < xs.length; j++) {
-                uint d = xs[i] + xs[j];
+        for (uint256 i = 0; i < xs.length; i++) {
+            for (uint256 j = i + 1; j < xs.length; j++) {
+                uint256 d = xs[i] + xs[j];
                 if (d <= 2020) {
-                    uint xk = 2020 - d;
+                    uint256 xk = 2020 - d;
                     if (m[xk] == true) {
                         return xs[i] * xs[j] * xk;
                     }
--- contracts/Parser.sol
+++ contracts/Parser.sol
@@ -6,31 +6,31 @@ contract Parser {
     ///
     /// 48 - '0'
     /// 57 - '9'
-    uint private constant ascii0 = 48;
-    uint private constant ascii9 = 57;
+    uint256 private constant ascii0 = 48;
+    uint256 private constant ascii9 = 57;
 
     /// Only storage arrays have a .push function, so we need to keep the
     /// internal array used by the parse methods as a state variable.
-    uint[] private xsStorage;
+    uint256[] private xsStorage;
 
     /// Convert the given string into an array of integers.
     ///
     /// Any non-digit character acts as a separator.
-    function parseInts(string memory s) internal returns (uint[] memory) {
+    function parseInts(string memory s) internal returns (uint256[] memory) {
         // Strings are not indexable.
         bytes memory b = bytes(s);
 
         // Clear the stored array, emptying it.
         delete xsStorage;
         // Get a local reference to the storage.
-        uint[] storage xs = xsStorage;
+        uint256[] storage xs = xsStorage;
 
-        uint x;
+        uint256 x;
         bool didSeeDigit = false;
-        for (uint i = 0; i < b.length; i++) {
+        for (uint256 i = 0; i < b.length; i++) {
             uint8 ascii = uint8(b[i]);
             if (ascii >= ascii0 && ascii <= ascii9) {
-                uint digit = ascii - ascii0;
+                uint256 digit = ascii - ascii0;
                 x *= 10;
                 x += digit;
 
