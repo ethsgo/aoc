740d5a0517033d1b00414aafe4e1c65a783c56fb
ethsgo <code@ethsgo.com>
2021-12-02 12:27:39 +0200

    Shorten example

--- contracts/BytesConcat.sol
+++ contracts/BytesConcat.sol
@@ -1,35 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/// Demonstrate the need to use bytes.contact instead of uint8[].push when
+/// Demonstrate the need to use bytes.contact instead of uint8[] when
 /// constructing strings from ASCII codepoints.
 contract BytesConcat {
-    function main() external {
-        require(parseDigit(parseToken("0")) == 0);
+    function main() external pure {
+        string memory s = parse("0");
+        require(uint8(bytes(s)[0]) == 48);
     }
 
-    uint8[] private tokenStorageUint8;
-
-    function parseToken(string memory s) internal returns (string memory) {
-        bytes memory b = bytes(s);
-
-        bytes memory tokenStorage;
-        for (uint256 i = 0; i < b.length; i++) {
-            uint8 ascii = uint8(b[i]);
-
-            // Need to use bytes.contact instead of creating an array of
-            // uint8 to ensure that the encoding is correct (otherwise the
-            // string indexing stops working later on down the line).
-            tokenStorage = bytes.concat(tokenStorage, bytes1(ascii));
-            tokenStorageUint8.push(ascii);
-        }
-
-        return string(tokenStorage);
-        // return string(abi.encodePacked(tokenStorageUint8));
-    }
-
-    function parseDigit(string memory s) internal pure returns (uint256) {
+    function parse(string memory s) internal pure returns (string memory) {
         uint8 ascii = uint8(bytes(s)[0]);
-        return ascii - 48; // 48 == '0'
+
+        // Need to use bytes.contact instead of creating an array of
+        // uint8 to ensure that the encoding is correct (otherwise the
+        // string indexing stops working later on down the line).
+
+        // This works
+        return string(bytes.concat(bytes1(ascii)));
+
+        // This doesn't
+        // return string(abi.encodePacked([ascii]));
     }
 }
