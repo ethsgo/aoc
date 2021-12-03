3127867def4c9f53ad878aa68fe3c0178b1ca491
ethsgo <code@ethsgo.com>
2021-12-03 08:59:39 +0200

    p2 js

--- js/_03.js
+++ js/_03.js
@@ -19,6 +19,13 @@ if (!process.stdin.isTTY) {
 
 const tokens = input.split(/\s+/).filter((t) => t.length > 0)
 
+function p1(tokens) {
+  const px = parity(tokens)
+  const bits = px.map((x) => x > 0)
+  const inverse = px.map((x) => x < 0)
+  return decimal(bits) * decimal(inverse)
+}
+
 // Positive if there are more 1s than 0s in the given index.
 function parity(tokens) {
   const len = tokens[0].length
@@ -38,39 +45,33 @@ const decimal = (bits) => bits.reduce((n, x) => n * 2 + (x ? 1 : 0))
 // Convert string bit array to a boolean bit array
 const boolBits = (bits) => [...bits].map((c) => c === '1')
 
-function p1(tokens) {
-  const px = parity(tokens)
-  const bits = px.map((x) => x > 0)
-  const inverse = px.map((x) => x < 0)
-  return decimal(bits) * decimal(inverse)
-}
-
 function p2(numbers) {
-  // If equal (parity == 0) then use 1
-  const mostCommon = filter(numbers, (parity) => parity >= 0)
-  // If equal (parity == 1) then use 0
-  const leastCommon = filter(numbers, (parity) => parity <= 0)
-  return decimal(boolBits(mostCommon))
+  // If equal (oneCount == 0) then use 1
+  const mostCommon = filter(numbers, (oneCount) => (oneCount >= 0 ? '1' : '0'))
+  // If equal (oneCount == 0) then use 0
+  const leastCommon = filter(numbers, (oneCount) => (oneCount < 0 ? '1' : '0'))
+  return decimal(boolBits(mostCommon)) * decimal(boolBits(leastCommon))
 }
 
 /// Filter down the given numbers (provided as bit strings of their
 /// binary representation) until we find a singleton that satisfies the given
 /// bit criteria.
 ///
-/// The criteria is passed the parity (positive value if there are more numbers
-/// with 1 in that bit position).
-function filter(numbers, criteria) {
+/// The bitCriteria is passed the oneCount (a positive value if there are more
+/// numbers with 1 in that bit position). It should return the bit that should
+/// be considered for that position.
+function filter(numbers, bitCriteria) {
   const len = numbers[0].length
 
   // For each bit position
   for (let j = 0; j < len; j += 1) {
     // Determine the most common bit in that position
-    let parity = 0
+    let oneCount = 0
     for (let i = 0; i < numbers.length; i++) {
-      parity += numbers[i][j] === '0' ? +1 : -1
+      oneCount += numbers[i][j] === '0' ? -1 : +1
     }
 
-    const bit = criteria(parity) ? '1' : '0'
+    const bit = bitCriteria(oneCount)
 
     // Keep only numbers that have that bit in that position
     numbers = numbers.filter((number) => number[j] == bit)
