cf6fbf66f7d54dc8330e2bf6864b5ed8dbf1ad53
ethsgo <code@ethsgo.com>
2021-12-03 10:25:46 +0200

    Fix example input

--- contracts/_03.sol
+++ contracts/_03.sol
@@ -8,7 +8,7 @@ contract _03 is Parser {
         string[] memory tokens = parseTokens(input);
         if (tokens.length == 0) {
             tokens = parseTokens(
-                "00100 11110 10110 10111 10101 01111 00111 11100"
+                "00100 11110 10110 10111 10101 01111 00111 11100 "
                 "10000 11001 00010 01010"
             );
         }
