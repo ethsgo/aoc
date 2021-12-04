7cb66deb81421d424a928ba7015229de69896ef2
ethsgo <code@ethsgo.com>
2021-12-04 17:31:31 +0200

    Simplify 03 js
    https://www.reddit.com/r/adventofcode/comments/r7r0ff/2021_day_3_solutions/hn4j604/

--- js/_03.js
+++ js/_03.js
@@ -42,43 +42,21 @@ function parity(tokens) {
 // Convert boolean bit array to a decimal number
 const decimal = (bits) => bits.reduce((n, x) => n * 2 + (x ? 1 : 0))
 
-// Convert string bit array to a boolean bit array
-const boolBits = (bits) => [...bits].map((c) => c === '1')
-
 function p2(numbers) {
-  // If equal (oneCount == 0) then use 1
-  const mostCommon = filter(numbers, (oneCount) => (oneCount >= 0 ? '1' : '0'))
-  // If equal (oneCount == 0) then use 0
-  const leastCommon = filter(numbers, (oneCount) => (oneCount < 0 ? '1' : '0'))
-  return decimal(boolBits(mostCommon)) * decimal(boolBits(leastCommon))
+  return reduce(numbers, 0, true) * reduce(numbers, 0, false)
 }
 
-/// Filter down the given numbers (provided as bit strings of their
-/// binary representation) until we find a singleton that satisfies the given
-/// bit criteria.
-///
-/// The bitCriteria is passed the oneCount (a positive value if there are more
-/// numbers with 1 in that bit position). It should return the bit that should
-/// be considered for that position.
-function filter(numbers, bitCriteria) {
-  const len = numbers[0].length
-
-  // For each bit position
-  for (let j = 0; j < len; j += 1) {
-    // Determine the most common bit in that position
-    let oneCount = 0
-    for (let i = 0; i < numbers.length; i++) {
-      oneCount += numbers[i][j] === '0' ? -1 : +1
-    }
-
-    const bit = bitCriteria(oneCount)
-
-    // Keep only numbers that have that bit in that position
-    numbers = numbers.filter((number) => number[j] == bit)
-    if (numbers.length == 1) return numbers[0]
+function reduce(numbers, i, mostCommon) {
+  if (numbers.length == 1) return parseInt(numbers[0], 2)
+  const zeroes = numbers.filter((n) => n[i] == '0')
+  const ones = numbers.filter((n) => n[i] === '1')
+  if (mostCommon) {
+    if (ones.length >= zeroes.length) return reduce(ones, i + 1, mostCommon)
+    else return reduce(zeroes, i + 1, mostCommon)
+  } else {
+    if (ones.length < zeroes.length) return reduce(ones, i + 1, mostCommon)
+    else return reduce(zeroes, i + 1, mostCommon)
   }
-
-  return ''
 }
 
 console.log(p1(tokens))
