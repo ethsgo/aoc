e80ad2c38877db3377ad74f1b03abc08056f1ad9
ethsgo <code@ethsgo.com>
2021-12-05 08:22:11 +0200

    bounds

--- contracts/_05.sol
+++ contracts/_05.sol
@@ -37,7 +37,61 @@ contract _05 is _05Parser {
         return (p1(parse(s)), 0);
     }
 
-    function p1(uint256[4][] memory segments) private returns (uint256) {
-        return segments.length;
+    function p1(uint256[4][] memory segments) private pure returns (uint256) {
+        segments = filterHVSegments(segments);
+        (uint256 maxX, uint256 maxY) = bounds(segments);
+
+        return maxX;
+    }
+
+    /// Return only horizontal or vertical segments from the given segments.
+    function filterHVSegments(uint256[4][] memory segments)
+        private
+        pure
+        returns (uint256[4][] memory)
+    {
+        // Dynamic memory arrays cannot be resized, so we need to iterate twice,
+        // first to find the count, and then to fill in the values.
+        uint256 c = 0;
+        for (uint256 i = 0; i < segments.length; i++) {
+            uint256[4] memory s = segments[i];
+            if (s[0] == s[2] || s[1] == s[3]) c++;
+        }
+        uint256[4][] memory result = new uint256[4][](c);
+        c = 0;
+        for (uint256 i = 0; i < segments.length; i++) {
+            uint256[4] memory s = segments[i];
+            if (s[0] == s[2] || s[1] == s[3]) result[c++] = s;
+        }
+        return result;
+    }
+
+    /// Return (maxX, maxY) from amongst the given segments.
+    function bounds(uint256[4][] memory segments)
+        private
+        pure
+        returns (uint256, uint256)
+    {
+        uint256 maxX;
+        uint256 maxY;
+        for (uint256 i = 0; i < segments.length; i++) {
+            uint256[4] memory s = segments[i];
+            maxX = max(maxX, s[0], s[2]);
+            maxY = max(maxY, s[1], s[3]);
+        }
+        return (maxX, maxY);
+    }
+
+    /// Return the maximum of up to three numbers
+    function max(
+        uint256 x,
+        uint256 y,
+        uint256 z
+    ) private pure returns (uint256) {
+        if (x >= y) {
+            return (x >= z) ? x : z;
+        } else {
+            return (y >= z) ? y : z;
+        }
     }
 }
