37baf5406c149cb3d469845b4b7035a7ffe77e47
ethsgo <code@ethsgo.com>
2021-12-07 08:35:10 +0200

    Closed form

--- js/_07.js
+++ js/_07.js
@@ -7,15 +7,20 @@ if (!process.stdin.isTTY) {
 const numbers = (s) => s.split(/[^\d.]+/).map(Number)
 
 const id = (n) => n
-const sumTo = (n) => (n == 0 ? 0 : sumTo(n - 1) + n)
+const sumTo = (n) => (n * (n + 1)) / 2
 
-const fuel = (crabs, position, scale) =>
-  crabs.map((x) => scale(Math.abs(x - position))).reduce((a, x) => a + x)
+const fuel = (crabs, position, scale) => {
+  const r = crabs
+    .map((x) => scale(Math.abs(x - position)))
+    .reduce((a, x) => a + x)
+  console.log(position, r)
+  return r
+}
 
 const p1 = (xs) => Math.min(...crabs.map((p) => fuel(crabs, p, id)))
-const p2 = (xs) => Math.min(...crabs.map((p) => fuel(crabs, p, sumTo)))
+const p2 = (xs) => crabs.map((p) => fuel(crabs, 5, sumTo))
 
 let crabs = numbers(input)
 
-console.log(p1(crabs))
+// console.log(p1(crabs))
 console.log(p2(crabs))
