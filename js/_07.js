49eb45ddf3ff140ab758cc89089e63de4b8fbd9c
ethsgo <code@ethsgo.com>
2021-12-07 08:27:01 +0200

    sumto

--- js/_07.js
+++ js/_07.js
@@ -6,12 +6,16 @@ if (!process.stdin.isTTY) {
 
 const numbers = (s) => s.split(/[^\d.]+/).map(Number)
 
-const fuel = (crabs, position) =>
-  crabs.map((x) => Math.abs(x - position)).reduce((a, x) => a + x)
+const id = (n) => n
+const sumTo = (n) => (n == 0 ? 0 : sumTo(n - 1) + n)
 
-const p1 = (xs) => Math.min(...crabs.map((p) => fuel(crabs, p)))
+const fuel = (crabs, position, scale) =>
+  crabs.map((x) => scale(Math.abs(x - position))).reduce((a, x) => a + x)
+
+const p1 = (xs) => Math.min(...crabs.map((p) => fuel(crabs, p, id)))
+const p2 = (xs) => Math.min(...crabs.map((p) => fuel(crabs, p, sumTo)))
 
 let crabs = numbers(input)
 
 console.log(p1(crabs))
-//console.log(p2(crabs))
+console.log(p2(crabs))
