cca9abd97db55a59c8149c7b71e87d26463fac8e
ethsgo <code@ethsgo.com>
2021-12-08 09:19:28 +0200

    Tinker 42

--- js/_08.js
+++ js/_08.js
@@ -21,6 +21,13 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
 fgae cfgab fg bagce
 `.trim()
 
+// Shorter input variation, useful for p2
+let inputShort = `
+acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
+cdfeb fcadb cdfeb cdbaf
+`.trim()
+input = inputShort
+
 if (!process.stdin.isTTY) {
   input = require('fs').readFileSync(0).toString().trim()
 }
@@ -49,13 +56,56 @@ function p1(ps) {
   }, 0)
 }
 
+const union = (a, b) => {
+  let u = new Set(a)
+  for (e of b) u.add(e)
+  return u
+}
 const intersection = (a, b) => new Set([...a].filter((e) => b.has(e)))
 const difference = (a, b) => new Set([...a].filter((e) => !b.has(e)))
 
-function p2() {
+function p2e(entry) {
+  // Segments of digits
+  let sofd = {}
+  // Candidates patterns of length 5 and 6
+  let c5 = []
+  let c6 = []
+  for (let p of entry.patterns) {
+    const len = p.length
+    const s = new Set(p)
+    if (len === 2) sofd[1] = s
+    if (len === 3) sofd[7] = s
+    if (len === 4) sofd[4] = s
+    if (len === 7) sofd[8] = s
+    if (len === 5) c5.push(s)
+    if (len === 6) c6.push(s)
+  }
+  console.log(sofd, c5, c6)
+  // Find the segments common in all patterns of length 5
+  const co5 = intersection(intersection(c5[0], c5[1]), c5[2])
+  // Cross these off in the segments in the pattern for 7. This eliminates
+  // vertical segments, and what is left is the right horizontal segments.
+  const hr5 = difference(sofd[7], co5)
+  // The pattern of length 5 has both the horizontal ones is the digit 3.
+  sofd[3] = union(co5, hr5)
+  // Of the remaining patterns of length 5, the one that shares a horizontal
+  // segment with 3 is the digit 2.
+  for (let s5 of c5) {
+    const h = difference(s5, co5)
+    console.log(difference(h, hr5))
+  }
+  //
+  //   console.log([...union(s5, hr5)].length)
+  //   if (union(s5, hr5) == s5)
+  // }
+  // The other
+  console.log(sofd[3])
   return 0
+}
+function p2(ps) {
+  return p2e(ps[0])
 }
 
 const ps = parse(input)
-console.log(p1(ps))
+// console.log(p1(ps))
 console.log(p2(ps))
