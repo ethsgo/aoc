9add85f1245fbd554bbfad9ff552a75dde26cc37
ethsgo <code@ethsgo.com>
2021-12-10 07:22:09 +0200

    p1 js

--- js/_10.js
+++ js/_10.js
@@ -17,10 +17,37 @@ if (!process.stdin.isTTY) {
 
 const parse = (input) => input.split(/\s+/)
 
-function p1(lines) {
-  return lines
+const p1 = (lines) => lines.map((s) => p1line(s)).reduce((a, x) => a + x)
+
+function p1line(s) {
+  let stack = []
+  for (const c of s) {
+    if (stack.length === 0) {
+      stack.push(c)
+      continue
+    }
+
+    if (c === ')') {
+      if (stack.pop() === '(') continue
+      else return 3
+    }
+    if (c === ']') {
+      if (stack.pop() === '[') continue
+      else return 57
+    }
+    if (c === '}') {
+      if (stack.pop() === '{') continue
+      else return 1197
+    }
+    if (c === '>') {
+      if (stack.pop() === '<') continue
+      else return 25137
+    }
+
+    stack.push(c)
+  }
+  return 0
 }
 
 const lines = parse(input)
 console.log(p1(lines))
-
