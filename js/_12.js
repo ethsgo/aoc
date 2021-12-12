600226facb0358149daf47476b235ee281efecf0
ethsgo <code@ethsgo.com>
2021-12-12 08:45:04 +0200

    clean

--- js/_12.js
+++ js/_12.js
@@ -48,84 +48,31 @@ if (!process.stdin.isTTY) {
 
 const parse = (input) => input.split(/\s+/).map((s) => s.split('-'))
 
-function p1a(links) {
-  let paths = new Set([['start']])
-  let c = 0
-  while (links.length > 0 && c++ < 5) {
-    console.log({ paths })
-    const heres = [...new Set([...paths].map((path) => path[path.length - 1]))]
-    for (const here of heres) {
-      console.log('here:', here)
-      const linksFromHere = links.filter((l) => l[0] === here)
-      console.log('linksFromHere:', linksFromHere)
-      links = links.filter(
-        (l) => !(l[0] === here && here.toUpperCase() !== here)
-      )
-      console.log('remaining links:', links)
-      let newPaths = []
-      for (const l of linksFromHere) {
-        for (const path of paths) {
-          if (path[path.length - 1] === here) {
-            newPaths.push([...path, l[1]])
-          }
-        }
-      }
-      console.log('newPaths:', newPaths)
-      for (const path of newPaths) {
-        paths.add(path)
-      }
-    }
-  }
-  return paths
-}
-
 function p1(links) {
-  // let visited = new Set()
   let frontier = [{ u: 'start', visited: new Set(), paths: [[]] }]
-  let parent = {}
-  // let paths = [['start']]
   let endedPaths = []
-  let allPaths = []
   while (frontier.length > 0) {
     let { u, visited, paths } = frontier.shift()
-    visited = new Set(visited)
-    paths = [...paths].map((path) => [...path, u])
-    allPaths = [...allPaths, ...paths]
-    // console.log({ allPaths })
 
-    // console.log('visit', u)
-    // If we're visiting a lowercase place, mark it as done.
+    visited = new Set(visited)
     if (u === u.toLowerCase()) visited.add(u)
-    // Where can we go next
+
+    paths = [...paths].map((path) => [...path, u])
+
     const next = [
       ...links.filter((link) => link[0] === u).map((link) => link[1]),
       ...links.filter((link) => link[1] === u).map((link) => link[0]),
     ]
+
     // console.log({ u, visited, next, paths })
-    // console.log({ next })
     for (const v of next) {
-      // Ignore places where we don't want to go to again
       if (visited.has(v) && v !== 'end') continue
-      let px = parent[v] ?? []
-      px.push(u)
-      parent[v] = px
-      let newPaths = []
-      // for (const path of paths) {
-      //   console.log({ path: path.join(','), u })
-      //   if (path[path.length - 1] === u) newPaths.push([...path, v])
-      // }
-      // paths = [...paths, ...newPaths]
       if (v === 'end') {
         endedPaths = [...endedPaths, ...paths.map((path) => [...path, 'end'])]
       } else {
-        // console.log({ v })
         frontier.push({ u: v, visited: new Set(visited), paths: [...paths] })
       }
     }
-    // console.log({ frontier })
-    // paths = [...new Set(paths)]
-    // console.log({ frontier })
-    // console.log({ paths })
   }
   let paths = endedPaths
   // paths = paths.filter((path) => path[path.length - 1] === 'end')
