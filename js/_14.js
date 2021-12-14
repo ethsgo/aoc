dec5771c795376e383ce6c6766c852e8f23ca5a9
ethsgo <code@ethsgo.com>
2021-12-14 08:02:05 +0200

    step

--- js/_14.js
+++ js/_14.js
@@ -26,13 +26,31 @@ if (!process.stdin.isTTY) {
 function parse(input) {
   const lines = input.split('\n')
   const template = lines[0]
-  const rules = lines.slice(2, lines.length).map((s) => s.split(/[ ->]+/))
+  const rulesKV = lines.slice(2, lines.length).map((s) => s.split(/[ ->]+/))
+  const rules = new Map(rulesKV)
   return { template, rules }
 }
 
 function p1({ template, rules }) {
-  let next = [...template]
-  return next
+  return step({ s: template, rules })
+}
+
+function step({ s, rules }) {
+  let next = []
+  let residue
+  for (let i = 0; i < s.length - 1; i++) {
+    const key = [s[i], s[i + 1]].join('')
+    const value = rules.get(key)
+    if (value) {
+      next.push([s[i], value].join(''))
+      residue = s[i + 1]
+    } else {
+      next.push(key)
+      residue = null
+    }
+  }
+  if (residue) next.push(residue)
+  return next.join('')
 }
 
 console.log(p1(parse(input)))
