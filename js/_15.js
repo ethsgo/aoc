f567ddfdb72ee99ee5a64fa5584ce5644967ea1a
ethsgo <code@ethsgo.com>
2021-12-15 07:31:02 +0200

    distances

--- js/_15.js
+++ js/_15.js
@@ -15,9 +15,32 @@ if (!process.stdin.isTTY) {
   input = require('fs').readFileSync(0).toString().trim()
 }
 
-const parse = (input) => input.split('\n').map(s => s.split('').map(Number))
+const parse = (input) => input.split('\n').map((s) => s.split('').map(Number))
 
-const p1 = (g) => g
+const p1 = (g) => shortestPath(g)
+
+function shortestPath(g) {
+  const ymax = g.length - 1
+  const xmax = g[ymax].length - 1
+
+  const neighbours = (x, y, w) =>
+    [
+      [x + 1, y],
+      [x - 1, y],
+      [x, y + 1],
+      [x, y - 1],
+    ]
+      .filter(([r, s]) => r >= 0 && s >= 0 && r <= xmax && s <= ymax)
+      .map(([x, y]) => [x, y, w + g[y][x]])
+
+  let distances = neighbours(0, 0, 0).sort((u, v) => u[2] - v[2])
+
+  while (distances.length > 0) {
+    let [x, y, w] = distances.pop()
+    console.log([x, y, w])
+    if (x === ymax && y === xmax) return w
+  }
+}
 
 const g = parse(input)
 console.log(p1(g))
