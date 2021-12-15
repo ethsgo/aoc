0cdf07ae58b5f1c546a4f1dbdc70e3e3707716ab
ethsgo <code@ethsgo.com>
2021-12-15 07:09:58 +0200

    Parse

--- js/_15.js
+++ js/_15.js
@@ -1,16 +1,23 @@
 let input = `
-
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
 `.trim()
 
 if (!process.stdin.isTTY) {
   input = require('fs').readFileSync(0).toString().trim()
 }
 
-function parse(input) {
-  
-}
+const parse = (input) => input.split('\n').map(s => s.split('').map(Number))
 
-const p1 = (pz) => pz
+const p1 = (g) => g
 
-const pz = parse(input)
-console.log(p1(pz))
+const g = parse(input)
+console.log(p1(g))
