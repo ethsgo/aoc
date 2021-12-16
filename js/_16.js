09acfe3c7eb77a76d260ae13f8c221ebe4d2fe78
ethsgo <code@ethsgo.com>
2021-12-16 08:40:20 +0200

    version sum

--- js/_16.js
+++ js/_16.js
@@ -6,7 +6,20 @@ const inputOpLen = `
 00111000000000000110111101000101001010010001001000000000
 `.trim()
 
-let input = inputOpLen
+const inputOpNum = `
+11101110000000001101010000001100100000100011000001100000
+`.trim()
+
+const hexToBin = (s) =>
+  s
+    .split('')
+    .map((c) => parseInt(c, 16).toString(2).padStart(4, 0))
+    .join('')
+
+const inputVsum16 = hexToBin('8A004A801A8002F478')
+const inputVSum31 = hexToBin('A0016C880162017C3686B18A3D4780')
+
+let input = inputVSum31
 
 if (!process.stdin.isTTY) {
   input = require('fs').readFileSync(0).toString().trim()
@@ -67,16 +80,34 @@ function operator(b) {
     let packets = []
     while (i < end) {
       const p = packet(b.slice(i, b.length))
-      console.log(p)
       packets.push(p.packet)
       i += p.length
     }
     return { operator: { id, packets }, length: i }
+  } else {
+    let n = decimal(b.slice(i, (i += 11)))
+    let packets = []
+    while (n > 0) {
+      const p = packet(b.slice(i, b.length))
+      packets.push(p.packet)
+      i += p.length
+      n--
+    }
+    return { operator: { id, packets }, length: i }
   }
-  return id
 }
 
-const p1 = (b) => packet(b)
+function versionSum(p) {
+  let c = p.version
+  if (p.packets) {
+    for (const x of p.packets) {
+      c += versionSum(x)
+    }
+  }
+  return c
+}
+
+const p1 = (b) => versionSum(packet(b).packet)
 
 const bits = parse(input)
 console.log(p1(bits))
