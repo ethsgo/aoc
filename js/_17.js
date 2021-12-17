e8c8d53006d4a4447dcc5e37defae30c453d3301
ethsgo <code@ethsgo.com>
2021-12-17 09:31:03 +0200

    p1

--- js/_17.js
+++ js/_17.js
@@ -13,23 +13,35 @@ const parse = (input) =>
 // ta: target area
 function valid(ta, v) {
   let p = [0, 0]
+  let ymax = 0
   while (p[0] <= ta[1] && p[1] >= ta[2]) {
     // console.log(p)
     const np = [p[0] + v[0], p[1] + v[1]]
     if (np[0] >= ta[0] && np[0] <= ta[1] && np[1] >= ta[2] && np[1] <= ta[3])
-      return true
+      return { hit: true, ymax }
+    ymax = Math.max(ymax, p[1])
     p = np
     v = [v[0] > 0 ? v[0] - 1 : v[0] < 0 ? v[0] + 1 : 0, v[1] - 1]
   }
-  return false
+  return { hit: false, ymax }
 }
 
 function trajectoryWithMaxY(ta) {
   // let v = [7, 2]
   // let v = [6, 3]
   // let v = [9, 0]
-  let v = [17, -4]
-  return valid(ta, v)
+  // let v = [17, -4]
+  let r = 0
+  let yr = Math.abs(ta[2])
+  for (let x = 0; x <= ta[1]; x++) {
+    for (let y = -yr; y <= yr; y++) {
+      const v = [x, y]
+      const { hit, ymax } = valid(ta, v)
+      // console.log(v, hit, ymax)
+      if (hit && ymax > r) r = ymax
+    }
+  }
+  return r
 }
 
 const p1 = (ta) => trajectoryWithMaxY(ta)
