b6f521ca87b00acfb41db4251779e6f0fa15135b
ethsgo <code@ethsgo.com>
2021-12-17 07:41:59 +0200

    p1 partial

--- js/_17.js
+++ js/_17.js
@@ -12,7 +12,17 @@ const parse = (input) =>
 
 // ta: target area
 function trajectoryWithMaxY(ta) {
-  return ta
+  let v = [7, 2]
+  let p = [0, 0]
+  while (p[0] <= ta[1] && p[1] >= ta[2]) {
+    console.log(p)
+    const np = [p[0] + v[0], p[1] + v[1]]
+    if (np[0] >= ta[0] && np[0] <= ta[1] && np[1] >= ta[2] && np[1] <= ta[3])
+      return true
+    p = np
+    v = [v[0] > 0 ? v[0] - 1 : v[0] < 0 ? v[0] + 1 : 0, v[1] - 1]
+  }
+  return false
 }
 
 const p1 = (ta) => trajectoryWithMaxY(ta)
