56ab8a37800837171b0794fc7047861030ad4763
ethsgo <code@ethsgo.com>
2021-12-17 09:49:45 +0200

    clean

--- js/_17.js
+++ js/_17.js
@@ -15,25 +15,24 @@ function valid(ta, v) {
   let p = [0, 0]
   let ymax = 0
   while (p[0] <= ta[1] && p[1] >= ta[2]) {
-    const np = [p[0] + v[0], p[1] + v[1]]
-    if (np[0] >= ta[0] && np[0] <= ta[1] && np[1] >= ta[2] && np[1] <= ta[3])
+    p[0] += v[0]
+    p[1] += v[1]
+    if (p[0] >= ta[0] && p[0] <= ta[1] && p[1] >= ta[2] && p[1] <= ta[3])
       return { hit: true, ymax }
     ymax = Math.max(ymax, p[1])
-    p = np
     v = [v[0] > 0 ? v[0] - 1 : v[0] < 0 ? v[0] + 1 : 0, v[1] - 1]
   }
   return { hit: false, ymax }
 }
 
-function trajectoryWithMaxY(ta) {
+function trajectories(ta) {
   let r = 0
   let c = 0
-  let yr = Math.abs(ta[2])
+  let yr = Math.abs()
   for (let x = 0; x <= ta[1]; x++) {
-    for (let y = -yr; y <= yr; y++) {
+    for (let y = ta[2]; y <= -ta[2]; y++) {
       const v = [x, y]
       const { hit, ymax } = valid(ta, v)
-      // console.log(v, hit, ymax)
       if (hit) c++
       if (hit && ymax > r) r = ymax
     }
@@ -41,7 +40,5 @@ function trajectoryWithMaxY(ta) {
   return [r, c]
 }
 
-const p1 = (ta) => trajectoryWithMaxY(ta)
-
 const ta = parse(input)
-console.log(p1(ta))
+console.log(trajectories(ta))
