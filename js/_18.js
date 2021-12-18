0748e74bc746b73590301dbd2c461a6b28b45a84
ethsgo <code@ethsgo.com>
2021-12-18 09:34:00 +0200

    explode

--- js/_18.js
+++ js/_18.js
@@ -10,38 +10,43 @@ if (!process.stdin.isTTY) {
 const parse = (input) => input.split('\n').map(JSON.parse)
 
 function linearize(n) {
+  function lin(n, depth) {
+    if (typeof n === 'number') {
+      return [[n, depth]]
+    } else {
+      const l = lin(n[0], depth + 1)
+      const r = lin(n[1], depth + 1)
+      return [...l, ...r]
+    }
+  }
+
   return lin(n, 0)
 }
 
-function lin(n, depth) {
-  if (typeof n === 'number') {
-    return [[n, depth]]
-  } else {
-    const l = lin(n[0], depth + 1)
-    const r = lin(n[1], depth + 1)
-    return [...l, ...r]
-  }
-}
-
 function explode(xs) {
-  for (let i = 0; i < xs.length; ) {
+  for (let i = 0; i < xs.length; i++) {
     let [v, depth] = xs[i]
     if (depth === 5) {
+      const xl = xs[i][0]
+      const xr = xs[i + 1][0]
       xs[i] = [0, 4]
       xs.splice(i + 1, 1)
-      i++
-    } else {
-      i++
+      if (i > 0) xs[i - 1][0] += xl
+      if (i + 1 < xs.length) xs[i + 1][0] += xr
+      return true
     }
   }
+  return false
 }
 
 function p1(ns) {
   const xs = linearize(ns)
-  explode(xs)
+  while (explode(xs)) {}
   return xs
 }
 
 const ns = parse(input)
 // console.log(p1(ns))
-console.log(p1([[[[[9, 8], 1], 2], 3], 4]))
+// console.log(p1([[[[[9, 8], 1], 2], 3], 4]))
+// console.log(p1([7,[6,[5,[4,[3,2]]]]]))
+console.log(p1([[6, [5, [4, [3, 2]]]], 1]))
