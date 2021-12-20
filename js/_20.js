292fce8e3267371ea7b08c64a80ba20d6a9c35d6
ethsgo <code@ethsgo.com>
2021-12-20 08:17:04 +0200

    wip

--- js/_20.js
+++ js/_20.js
@@ -47,9 +47,9 @@ function show(image) {
 
 /// Return a new image by padding the given image by 4 pixels on each side.
 function padded(image) {
-  let w = image[0].length
-  let h = image.length
-  let emptyRow = [...Array(w + 2 * 4)].map((_) => '.')
+  const w = image[0].length
+  const h = image.length
+  const emptyRow = [...Array(w + 2 * 4)].map((_) => '.')
 
   let newImage = []
   for (let i = 0; i < h + 2 * 4; i++) {
@@ -63,10 +63,44 @@ function padded(image) {
   return newImage
 }
 
+const copy = (image) => image.map((row) => [...row])
+
+function decimal(pixels) {
+  let d = 0
+  console.log(pixels)
+  for (const p of pixels) {
+    console.log(p, d)
+    d *= 2
+    if (p === '#') d++
+  }
+  return d
+}
+
 function p1({ map, image }) {
   show(image)
-  let newImage = padded(image)
-  show(newImage)
+
+  const base = padded(image)
+  let next = copy(base)
+
+  const w = base[0].length
+  const h = base.length
+
+  // console.log(decimal('...#...#.'.split('')), 34)
+
+  for (let y = 4; y < h - 4; y++) {
+    for (let x = 4; x < w - 4; x++) {
+      // prettier-ignore
+      let pixels = [
+        base[y - 1][x - 1], base[y - 1][x], base[y - 1][x + 1],
+        base[y + 0][x - 1], base[y + 0][x], base[y + 0][x + 1],
+        base[y + 1][x - 1], base[y + 1][x], base[y + 1][x + 1],
+      ]
+      next[y][x] = map[decimal(pixels)]
+    }
+  }
+
+  show(next)
+  show(base)
 }
 
 const data = parse(input)
