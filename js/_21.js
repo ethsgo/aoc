c51a89e0bf6318202f973cfbc924e51e9f5a28af
ethsgo <code@ethsgo.com>
2021-12-21 07:54:53 +0200

    take 2

--- js/_21.js
+++ js/_21.js
@@ -10,22 +10,26 @@ if (!process.stdin.isTTY) {
 const parse = (input) => input.split('\n').map((s) => Number(s.split(':')[1]))
 
 const wrap = (i) => (i > 10 ? i - 10 : i)
+
 function game(i1, i2) {
   let dice = 1
   let s1 = 0
   let s2 = 0
   let rolls = 0
+  function move(i) {
+    const d = dice + dice + 1 + dice + 2
+    dice = dice + 3
+    const j = wrap(i + (d % 10))
+    console.log({ i, j, d, dice })
+    return j
+  }
   while (true) {
-    i1 = wrap(i1 + dice++)
-    i1 = wrap(i1 + dice++)
-    i1 = wrap(i1 + dice++)
+    i1 = move(i1)
     s1 += i1
     rolls += 3
     console.log('player 1 score', s1, i1)
     if (s1 >= 1000) break
-    i2 = wrap(i2 + dice++)
-    i2 = wrap(i2 + dice++)
-    i2 = wrap(i2 + dice++)
+    i2 = move(i2)
     s2 += i2
     console.log('player 2 score', s2, i2)
     rolls += 3
