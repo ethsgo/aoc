b6c407045c732a58def41f73bc0bc93b9c4d1eb5
ethsgo <code@ethsgo.com>
2021-12-21 08:11:07 +0200

    clean

--- js/_21.js
+++ js/_21.js
@@ -11,7 +11,7 @@ const parse = (input) => input.split('\n').map((s) => Number(s.split(':')[1]))
 
 const wrap = (i) => (i > 10 ? i - 10 : i)
 
-function game(i1, i2) {
+function gameD(i1, i2) {
   let dice = 1
   let s1 = 0
   let s2 = 0
@@ -19,26 +19,45 @@ function game(i1, i2) {
   function move(i) {
     const d = dice + dice + 1 + dice + 2
     dice = dice + 3
-    const j = wrap(i + (d % 10))
-    console.log({ i, j, d, dice })
-    return j
+    rolls += 3
+    return wrap(i + (d % 10))
   }
   while (true) {
     i1 = move(i1)
     s1 += i1
-    rolls += 3
-    console.log('player 1 score', s1, i1)
-    if (s1 >= 1000) break
+    if (s1 >= 1000) return s2 * rolls
     i2 = move(i2)
     s2 += i2
-    console.log('player 2 score', s2, i2)
-    rolls += 3
-    if (s2 >= 1000) break
+    if (s2 >= 1000) return s1 * rolls
   }
-  return (s1 >= 1000 ? s2 : s1) * rolls
 }
 
-const p1 = (data) => game(...data)
+function gameQ(i1, i2) {
+  return _gameQ(i1, i2, 0, 0)
+}
+
+function _gameQ(i1, i2, s1, s2) {
+  let dice = 1
+  let rolls = 0
+  function move(i) {
+    const d = dice + dice + 1 + dice + 2
+    dice = dice + 3
+    rolls += 3
+    return wrap(i + (d % 10))
+  }
+  while (true) {
+    i1 = move(i1)
+    s1 += i1
+    if (s1 >= 1000) return s2 * rolls
+    i2 = move(i2)
+    s2 += i2
+    if (s2 >= 1000) return s1 * rolls
+  }
+}
+
+const p1 = (data) => gameD(...data)
+const p2 = (data) => gameQ(...data)
 
 const data = parse(input)
 console.log(p1(data))
+console.log(p2(data))
