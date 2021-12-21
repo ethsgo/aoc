8c980bea1691e52923820e00f8f2d66999992517
ethsgo <code@ethsgo.com>
2021-12-21 12:38:48 +0200

    Try to speed up p2

--- contracts/_21.sol
+++ contracts/_21.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "./Parser.sol";
+import "hardhat/console.sol";
 
 contract _21Parser is Parser {
     string private constant exampleInput =
@@ -24,7 +25,10 @@ contract _21Parser is Parser {
 contract _21 is _21Parser {
     function main(string calldata input) external returns (uint256, uint256) {
         uint256[2] memory pos = parse(input);
-        return (p1(pos), p2(pos));
+        return (
+            0, /*p1(pos)*/
+            p2(pos)
+        );
     }
 
     function p1(uint256[2] memory initialPos) private pure returns (uint256) {
@@ -58,34 +62,52 @@ contract _21 is _21Parser {
     }
 
     function p2(uint256[2] memory pos) private returns (uint256) {
-        uint256[2] memory w = winCount(pos, [uint256(0), 0], 0);
+        uint256[2] memory w = winCount(pos[0], pos[1], 0, 0, 0);
         return w[0] > w[1] ? w[0] : w[1];
     }
 
-    mapping(bytes32 => uint256[2]) memo;
+    mapping(uint256 => uint256[2]) memo;
 
-    function mkey(
-        uint256[2] memory pos,
-        uint256[2] memory score,
+    // ~ 1.5x slower than the hand rolled "hash".
+    function mkey2(
+        uint256 i0,
+        uint256 i1,
+        uint256 s0,
+        uint256 s1,
         uint256 pi
     ) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(i0, i1, s0, s1, pi));
+    }
+
+    function mkey(
+        uint256 i0,
+        uint256 i1,
+        uint256 s0,
+        uint256 s1,
+        uint256 pi
+    ) private pure returns (uint256) {
         // Use an arbitrary but unique mapping of the arguments to uints, for
         // use as the key to our memoized function results.
-        return keccak256(abi.encode(pos, score, pi));
+        return (100 * ((100 * ((100 * ((100 * i0) + i1)) + s0)) + s1)) + pi;
     }
 
     function winCount(
-        uint256[2] memory pos,
-        uint256[2] memory score,
+        uint256 i0,
+        uint256 i1,
+        uint256 s0,
+        uint256 s1,
         uint256 pi
     ) private returns (uint256[2] memory) {
-        bytes32 key = mkey(pos, score, pi);
+        uint256 key = mkey(i0, i1, s0, s1, pi);
         uint256[2] memory v = memo[key];
-        if (v[0] != 0 && v[1] != 0) return v;
+        if (v[0] != 0 && v[1] != 0) {
+            // console.log('hit', key);
+            return v;
+        }
 
-        if (score[0] >= 21) {
+        if (s0 >= 8) {
             v = [uint256(1), 0];
-        } else if (score[1] >= 21) {
+        } else if (s1 >= 8) {
             v = [uint256(0), 1];
         } else {
             v = [uint256(0), 0];
@@ -93,14 +115,18 @@ contract _21 is _21Parser {
             for (uint256 d1 = 1; d1 <= 3; d1++) {
                 for (uint256 d2 = 1; d2 <= 3; d2++) {
                     for (uint256 d3 = 1; d3 <= 3; d3++) {
-                        uint256[2] memory pc = [pos[0], pos[1]];
-                        uint256[2] memory sc = [score[0], score[1]];
-                        uint256 ni = pi == 0 ? 1 : 0;
+                        uint256 i = pi == 0 ? i0 : i1;
+                        uint256 s = pi == 0 ? s0 : s1;
+                        i = advance(i, d1, d2, d3);
+                        s += i;
 
-                        pc[pi] = advance(pc[pi], d1, d2, d3);
-                        sc[pi] += pc[pi];
+                        uint256[2] memory w;
+                        if (pi == 0) {
+                            w = winCount(i, i1, s, s1, 1);
+                        } else {
+                            w = winCount(i0, i, s0, s, 0);
+                        }
 
-                        uint256[2] memory w = winCount(pc, sc, ni);
                         v[0] += w[0];
                         v[1] += w[1];
                     }
