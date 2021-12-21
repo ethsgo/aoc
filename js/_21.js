bf92672d9e1dafaa7c1d9a4eee9203364f1f788c
ethsgo <code@ethsgo.com>
2021-12-21 07:30:01 +0200

    Parse

---
+++ js/_21.js
@@ -0,0 +1,15 @@
+let input = `
+Player 1 starting position: 4
+Player 2 starting position: 8
+`.trim()
+
+if (!process.stdin.isTTY) {
+  input = require('fs').readFileSync(0).toString().trim()
+}
+
+const parse = (input) => input.split('\n').map((s) => Number(s.split(':')[1]))
+
+const p1 = (data) => data
+
+const data = parse(input)
+console.log(p1(data))
